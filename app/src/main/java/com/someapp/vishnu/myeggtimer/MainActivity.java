54c7a71f7b9866ba6b75811ec4a5f02e831164fd
Vishnu Adda <addavishnu@gmail.com>
2018-05-28 23:05:03 -0700

    General layout has been determined.

--- app/src/main/java/com/someapp/vishnu/myeggtimer/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/myeggtimer/MainActivity.java
@@ -2,12 +2,19 @@ package com.someapp.vishnu.myeggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String timeString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView timeStringView = findViewById(R.id.textView);
+        timeString = timeStringView.toString();
+
     }
 }
