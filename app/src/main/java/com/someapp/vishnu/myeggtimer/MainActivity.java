ff4ef9d25c46e43f3dd5dbde37d072079afcc5bb
Vishnu Adda <addavishnu@gmail.com>
2018-05-28 23:41:20 -0700

    Basically did everything, but there's an issue where the CountDownTimer goes faster when I pause and play again.

--- app/src/main/java/com/someapp/vishnu/myeggtimer/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/myeggtimer/MainActivity.java
@@ -1,20 +1,121 @@
 package com.someapp.vishnu.myeggtimer;
 
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String timeString;
+    TextView timeStringView;
+    SeekBar timeControl;
+    MediaPlayer mediaPlayer;
+
+    int timeInSeconds;
+    boolean isStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView timeStringView = findViewById(R.id.textView);
-        timeString = timeStringView.toString();
+        timeInSeconds = 0;
+
+        timeControl = findViewById(R.id.seekBar);
+
+        timeControl.setMax(600);
+
+        timeStringView = findViewById(R.id.textView);
+        timeString = timeStringView.getText().toString();
+
+        Toast.makeText(this,timeString, Toast.LENGTH_SHORT).show();
+
+        timeControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                timeInSeconds = progress;
+
+                if((timeInSeconds % 60) < 10) {
+                    timeString = timeInSeconds/60 + ":0" + timeInSeconds%60;
+                } else {
+                    timeString = timeInSeconds/60 + ":" + timeInSeconds%60;
+                }
+
+                timeStringView.setText(timeString);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(final SeekBar seekBar) {
+
+                new CountDownTimer( timeInSeconds*1000, 1000) {
+
+                    public void onTick(long millisecondsUntilDone) {
+
+                        if(!isStop) return;
+
+                        timeInSeconds -= 1;
+
+                        seekBar.setProgress(timeInSeconds);
+
+                        if((seekBar.getProgress() % 60) < 10) {
+                            timeString = seekBar.getProgress()/60 + ":0" + seekBar.getProgress()%60;
+                        } else {
+                            timeString = seekBar.getProgress()/60 + ":" + seekBar.getProgress()%60;
+                        }
+
+                        timeStringView.setText(timeString);
+
+                    }
+
+                    public void onFinish() {
+
+                        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+
+                        mediaPlayer.start();
+
+                        Toast.makeText(getApplicationContext(),
+                                "done", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }.start();
+
+            }
+        });
+
+    }
+
+    public void onClick(View view) {
+
+        Button button = findViewById(R.id.button);
+
+        if(!isStop) {
+
+            isStop = true;
+            button.setText("Stop!");
+            timeControl.setEnabled(false);
+
+        } else {
+
+            isStop = false;
+            button.setText("Go!");
+            timeControl.setEnabled(true);
+
+        }
 
     }
 }
